37351e2ae47f0b8f634224a109d21586ed110fc4
Md. Zakaria Ahnaf <zakaria112203@gmail.com>
2024-06-02 17:13:01 +0600

    day2 - 19-Feb-2023, 20Feb- Otp, Post Request, 22-DashBoard Design full end.
    26-Feb-2023 - Sync Page added. Doctor database Created. stored sql data sqflite.change. delete and insert, data duplicate not solve, data duplicate solve. solve 1, Store Data in sql, update some, error, depot add database. Login api pass data.
    
    > Login Shareprefarance
    > State Save
    > Logout Dialog
    > error solve
    > get data from sqlite
    > doctor list get from local db
    > added autocompleatesd
    > Dropdown Search, group, product, dialog item added group wise, search add, add qty

--- lib/order_functionality/dialog_custom.dart
+++ lib/order_functionality/dialog_custom.dart
@@ -2,58 +2,130 @@ import 'package:deeplaid_apps_models/model/item_model.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 
-
-void showCustomDialog(BuildContext context, List<ItemModel> userProducetsGroupWise) {
-  TextEditingController searchController = TextEditingController();
+void showCustomDialog(
+    BuildContext context, List<ItemModel> userProducetsGroupWise) {
   showDialog(
     context: context,
     builder: (BuildContext context) {
-      return AlertDialog(
-        content: Container(
-          width: double.maxFinite,
-          child: Column(
-            children: [
-              TextField(
-                controller: searchController,
-                decoration: InputDecoration(
-                  labelText: 'Search',
-                  prefixIcon: Icon(Icons.search),
-                ),
-              ),
-              Expanded(
-                child: ListView.builder(
-                  shrinkWrap: true,
-                  itemCount: userProducetsGroupWise.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    return ListTile(
-                      title: Text(userProducetsGroupWise[index].itemName.toString()),
-                      onTap: () {
-                        Navigator.of(context).pop();
-                        // Handle item selection
-                        print('Selected Item ${userProducetsGroupWise[index].itemName}');
-                      },
-                    );
-                  },
-                ),
-              ),
-            ],
-          ),
-        ),
-        actions: <Widget>[
-          TextButton(
-            child: Text('Add Product'),
-            onPressed: () {
-              Fluttertoast.showToast(msg: "add item");
-            },
-          ),
-          // TextButton(
-          //   child: Text('Close'),
-          //   onPressed: () {
-          //     Navigator.of(context).pop();
-          //   },
-          // ),
-        ],
-      );
+      return CustomDialog(userProducetsGroupWise: userProducetsGroupWise);
     },
   );
+}
+
+class CustomDialog extends StatefulWidget {
+  final List<ItemModel> userProducetsGroupWise;
+
+  CustomDialog({required this.userProducetsGroupWise});
+
+  @override
+  _CustomDialogState createState() => _CustomDialogState();
+}
+
+class _CustomDialogState extends State<CustomDialog> {
+  TextEditingController searchController = TextEditingController();
+  TextEditingController quantityController = TextEditingController();
+  List<ItemModel> filteredList = [];
+
+  @override
+  void initState() {
+    super.initState();
+    filteredList = widget.userProducetsGroupWise;
+    searchController.addListener(_filterList);
+  }
+
+  void _filterList() {
+    String query = searchController.text.toLowerCase();
+    setState(() {
+      filteredList = widget.userProducetsGroupWise.where((item) {
+        return item.itemName!.toLowerCase().contains(query);
+      }).toList();
+    });
+  }
+
+  @override
+  void dispose() {
+    searchController.removeListener(_filterList);
+    searchController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      content: Container(
+        width: double.maxFinite,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Padding(
+              padding: EdgeInsets.all(6.0), // Add padding around the TextField
+              child: TextField(
+                controller: searchController,
+                decoration: const InputDecoration(
+                  labelText: 'Search Item',
+                  prefixIconColor: Colors.grey,
+                  prefixIcon: Icon(Icons.search, color: Colors.black),
+                  contentPadding:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
+                  // Add padding inside the TextField
+                  border:
+                      OutlineInputBorder(), // Optional: Add border to the TextField
+                ),
+              ),
+            ),
+            Expanded(
+              child: ListView.builder(
+                shrinkWrap: true,
+                itemCount: filteredList.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return ListTile(
+                    title: Row(
+                      children: [
+                        Expanded(
+                          child: Text(filteredList[index].itemName.toString()),
+                        ),
+                        Container(
+                          width: 50,
+                          child: TextField(
+                            controller: quantityController,
+                            keyboardType: TextInputType.number,
+                            decoration: InputDecoration(
+                              hintText: 'Qty',
+                              contentPadding: EdgeInsets.symmetric(
+                                  vertical: 10.0, horizontal: 10.0),
+                              border: OutlineInputBorder(),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    onTap: () {
+                      Navigator.of(context).pop();
+                      // Handle item selection
+                      print('Selected Item ${filteredList[index].itemName}');
+                    },
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+      actions: <Widget>[
+        TextButton(
+          child: Text('Close'),
+          onPressed: () {
+            Navigator.of(context).pop();
+            //Fluttertoast.showToast(msg: "Add item");
+          },
+        ),
+        TextButton(
+          child: Text('Add Product'),
+          onPressed: () {
+            Fluttertoast.showToast(msg: "Add item");
+          },
+        ),
+      ],
+    );
+  }
 }
