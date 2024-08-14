9333121c4d7c000e77d8ee615d03123a42ddac44
Md. Zakaria Ahnaf <zakaria112203@gmail.com>
2024-08-14 18:01:47 +0600

    day2 - 19-Feb-2023, 20Feb- Otp, Post Request, 22-DashBoard Design full end.
    26-Feb-2023 - Sync Page added. Doctor database Created. stored sql data sqflite.change. delete and insert, data duplicate not solve, data duplicate solve. solve 1, Store Data in sql, update some, error, depot add database. Login api pass data.
    
    > Login Shareprefarance
    > State Save
    > Logout Dialog
    custom design
    > error solve
    > get data from sqlite
    > doctor list get from local db
    > added autocompleatesd
    > Dropdown Search, group, product, dialog item added group wise, search add, add qty system, home, products, order list add, details order created, login table created file, text design
    
    tab solved
    
    dashboard desing ios, update, tabLayout

--- lib/dashboard_report/dashboard_activity.dart
+++ lib/dashboard_report/dashboard_activity.dart
@@ -57,7 +57,7 @@ class _DashboardActivityState extends State<DashboardActivity>
 
   @override
   void initState() {
-    _tabController = TabController(length: 3, vsync: this);
+    _tabController = TabController(length: 5, vsync: this);
     super.initState();
   }
 
